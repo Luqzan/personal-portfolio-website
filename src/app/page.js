d1ff24aab07656b2e957a86bdbe0c80dcb95dad6
Luqzan Ariff <luqzanariff@gmail.com>
2024-10-24 15:37:48 +0800

    story: first draft

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,67 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className="text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          Hi sayang!
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20">
+      <header></header>
+
+      <main className="">
+        <p className="text-center sm:text-left font-mono text-4xl font-thin">
+          Here is my story:
         </p>
+
+        <p>My name is Luqzan Ariff,</p>
+
+        <p>Born and raised in Malaysia.</p>
+
+        <p>But I struggled.</p>
+
+        <p>Struggled finding my way.</p>
+
+        <p>Struggled finding my place.</p>
+
+        <p>So, I asked myself:</p>
+
+        <p>&quot;What do I love?&quot;</p>
+
+        <p>&quot;What&apos;s my passion?&quot;</p>
+
+        <p>Then I realized.</p>
+
+        <p>
+          Ever since a child, I&apos;ve always been fascinated by technologies.
+        </p>
+
+        <p>I love tinkering with toys.</p>
+
+        <p>I love taking things apart and put them back together.</p>
+
+        <p>
+          Like the old PlayStation2 my family had that broken down because of
+          me.
+        </p>
+
+        <p>I love computers.</p>
+
+        <p>When I was 12, I got my first desktop computer.</p>
+
+        <p>I love the adventures of upgrading it.</p>
+
+        <p>Thank you, Linus Tech Tips for inspiring me.</p>
+
+        <p>In pursuit of finding my place in the world,</p>
+
+        <p>I discovered a whole new world of digital development.</p>
+
+        <p>I started learning, and continued grinding, despite all odds.</p>
+
+        <p>Because finally, I&apos;ve found my calling.</p>
+
+        <p>I know that I still have a long journey.</p>
+
+        <p>With you, we can make it a meaningful one.</p>
       </main>
+
+      <footer></footer>
     </div>
   );
 }
